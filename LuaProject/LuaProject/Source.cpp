db8b3393480588a7cce817eb1e02b533b4194b1e
Peter Aoun <peao14@student.bth.se>
2016-04-22 17:35:14 +0200

    Added sfml to project

--- LuaProject/LuaProject/Source.cpp
+++ LuaProject/LuaProject/Source.cpp
@@ -1,4 +1,5 @@
 #include "LuaHandler.h"
+#include "include\SFML\Graphics.hpp"
 
 int main()
 {
@@ -6,6 +7,24 @@ int main()
 
 	LuaHandler lh;
 	lh.start("test.lua");
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
 	std::system("pause");
 	return 0;
